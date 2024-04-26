c9939268dd048ab080291ffb8201ef4c7345c35c
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 15:46:11 +0700

    lagii

--- TEMPLATE/js/fetch.js
+++ TEMPLATE/js/fetch.js
@@ -3,7 +3,7 @@ import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "./table.js";
-let urlAPI = "hhttps://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
+let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
     results.forEach(isiRow);
