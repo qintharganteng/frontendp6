e20507763af77dcd560c0a77be14a8494313769b
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 15:26:58 +0700

    lagi

--- TEMPLATE/js/fetch.js
+++ TEMPLATE/js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/";
+let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
