dbf82265a5b90bc766d7e6f169d4728e0e5ddc22
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 16:15:19 +0700

    lagi

---
+++ TEMPLATE/js/config/url.js
@@ -0,0 +1,1 @@
+export let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
--- TEMPLATE/js/contoller/get.js
+++ TEMPLATE/js/contoller/get.js
@@ -1,22 +1,21 @@
-
-import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "./table.js";
-let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
-get(urlAPI, isiTablePresensi);
-function isiTablePresensi(results) {
+import { isiTabel } from "../temp/table.js";
+export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
 function isiRow(value) {
-    let content = 
-    isiTabel.replace("#NAMA#", value.biodata.nama)
+    let content =
+        isiTabel.replace("#NAMA#", value.biodata.nama)
             .replace("#NOHP#", value.biodata.phone_number)
             .replace("#JABATAN#", value.biodata.jabatan)
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
             .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#JAMKERJA#", value.biodata.jam_kerja? value.biodata.jam_kerja[0].durasi + "jam" : "Belum diatur") 
+            .replace("#JAMMASUK#", value.biodata.jam_kerja? value.biodata.jam_kerja[0].jam_masuk + "WIB" : "Belum diatur") 
+            .replace("#JAMKELUAR#", value.biodata.jam_kerja? value.biodata.jam_kerja[0].jam_keluar + "WIB" : "Belum diatur")
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
-        addInner("iniTabel", content);
+    addInner("iniTabel", content);
 }
